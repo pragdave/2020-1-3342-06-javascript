c81cee410748602131369399ea5583c69103faf1
Manqi Shi <Manqi@Manqis-MacBook-Air-3.local>
2020-02-26 22:17:47 -0600

    1.26.20 q3 error

--- assignment.js
+++ assignment.js
@@ -47,11 +47,11 @@ Error.stackTraceLimit = 2                 // set up the environment
 
 let a = 5
 // START
-let b
-let c
-let d
-let e
-let f
+let b = 10
+let c = -11
+let d = "hawaii"
+let e = "99"
+let f = "0"
 // END
 
 assert.equal(a + b, 15)
@@ -62,10 +62,8 @@ assert.equal(d + a + f, "hawaii50")
 
 // add parentheses to the first parameter to make this pass
 // START
-assert.equal(d + b + c, "hawaii-1")
+assert.equal(d + (b + c), "hawaii-1")
 //
-
-if (false) { // move me down to the next section when the one above is complete
 
 ///////////////// Section 2 ///////////////////////////////////////////
 //
@@ -74,11 +72,10 @@ if (false) { // move me down to the next section when the one above is complete
 // START
 let sum = 0
 for (let i = 1; i <= 10; i++) {
-  let sum = sum + i
+   sum = sum + i
 }
 // END
 assert.equal(sum, 55)
-
 
 ///////////////// Section 3 ///////////////////////////////////////////
 //
@@ -86,14 +83,11 @@ assert.equal(sum, 55)
 
 let result = []
 //START
-for (let i in [1,2,3,4]) {
+for (let i of [1,2,3,4]) { //i = 1; i < 5; i++
 // END
   result.push(3*i)
 }
 assert.equal(result, [3,6,9,12])
-
-
-
 
 ///////////////// Section 4 ///////////////////////////////////////////
 //
@@ -116,6 +110,7 @@ for (let city in  populations) {
 }
 assert.equal(total, 25_145_561)
 
+if (false) { // move me down to the next section when the one above is complete
 
 ///////////////// Section 5 ///////////////////////////////////////////
 //
