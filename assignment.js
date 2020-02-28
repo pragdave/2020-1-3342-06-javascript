a953e45b2fc28afe84305a2c65e2844196b78bf0
Manqi Shi <Manqi@Manqis-MacBook-Air-3.local>
2020-02-27 20:59:32 -0600

    1.27.20 3 working; need to check 6&7

--- assignment.js
+++ assignment.js
@@ -87,7 +87,7 @@ for (let i of [1,2,3,4]) { //i = 1; i < 5; i++
 // END
   result.push(3*i)
 }
-assert.equal(result, [3,6,9,12])
+assert.deepEqual(result, [3,6,9,12])
 
 if (false) { // move me down to the next section when the one above is complete
 
