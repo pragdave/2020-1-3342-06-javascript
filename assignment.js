475b06caa7cf5455050b825448cece92b39736f1
Harrison Noble <harrisonnoble@yahoo.com>
2020-03-01 15:07:12 -0600

    Section 6 done. All tests passed.

--- assignment.js
+++ assignment.js
@@ -124,8 +124,6 @@ assert.equal(nextLabel("entry"), "entry-1")
 assert.equal(nextLabel("entry"), "entry-2")
 assert.equal(nextLabel("exit"),  "exit-3")
 
-if (false) { // move me down to the next section when the one above is complete
-
 ///////////////// Section 6 ///////////////////////////////////////////
 //
 // The problem with `nextLabel` is that it uses a global
@@ -136,10 +134,12 @@ if (false) { // move me down to the next section when the one above is complete
 // a new function for each label to be seauenced
 
 function labelMaker(label) {
+  // START
   let count = 0;
-  return (label) => {
+  return () => {
     return label + '-' + count++;
   }
+  // END
 }
 
 let nextEntryLabel = labelMaker("entry")
@@ -150,6 +150,8 @@ assert.equal(nextEntryLabel(), "entry-1")
 assert.equal(nextExitLabel(),  "exit-0")
 assert.equal(nextEntryLabel(), "entry-2")
 assert.equal(nextExitLabel(),  "exit-1")
+
+if (false) { // move me down to the next section when the one above is complete
 
 ///////////////// Section 7 ///////////////////////////////////////////
 //
