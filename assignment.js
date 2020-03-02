a9103fa700d5acb4788715e80d3dda6eec66812e
mitchmorrison2 <mitchm@smu.edu>
2020-03-01 18:39:35 -0600

    tests 1 - 5

--- assignment.js
+++ assignment.js
@@ -47,11 +47,11 @@ Error.stackTraceLimit = 2                 // set up the environment
 
 let a = 5
 // START
-let b
-let c
-let d
-let e
-let f
+let b = 10
+let c = -11
+let d = "hawaii"
+let e = 99
+let f = 0
 // END
 
 assert.equal(a + b, 15)
@@ -62,10 +62,8 @@ assert.equal(d + a + f, "hawaii50")
 
 // add parentheses to the first parameter to make this pass
 // START
-assert.equal(d + b + c, "hawaii-1")
+assert.equal(d + (b + c), "hawaii-1")
 //
-
-if (false) { // move me down to the next section when the one above is complete
 
 ///////////////// Section 2 ///////////////////////////////////////////
 //
@@ -74,7 +72,7 @@ if (false) { // move me down to the next section when the one above is complete
 // START
 let sum = 0
 for (let i = 1; i <= 10; i++) {
-  let sum = sum + i
+    sum = sum + i
 }
 // END
 assert.equal(sum, 55)
@@ -86,13 +84,11 @@ assert.equal(sum, 55)
 
 let result = []
 //START
-for (let i in [1,2,3,4]) {
+for (let i of [1,2,3,4]) {
 // END
   result.push(3*i)
 }
 assert.deepEqual(result, [3,6,9,12])
-
-
 
 
 ///////////////// Section 4 ///////////////////////////////////////////
@@ -111,11 +107,10 @@ let populations = {
 let total = 0
 for (let city in  populations) {
   //START
-  total += populations.city
+  total += populations[city] //using city as key to access value
   // END
 }
 assert.equal(total, 25_145_561)
-
 
 ///////////////// Section 5 ///////////////////////////////////////////
 //
@@ -124,7 +119,7 @@ assert.equal(total, 25_145_561)
 let count = 0
 function nextLabel(label) {
   // START
-  // ...
+  return label+"-"+count++
   // END
 }
 
@@ -132,6 +127,7 @@ assert.equal(nextLabel("entry"), "entry-0")
 assert.equal(nextLabel("entry"), "entry-1")
 assert.equal(nextLabel("entry"), "entry-2")
 assert.equal(nextLabel("exit"),  "exit-3")
+
 
 
 ///////////////// Section 6 ///////////////////////////////////////////
@@ -157,6 +153,8 @@ assert.equal(nextEntryLabel(), "entry-1")
 assert.equal(nextExitLabel(),  "exit-0")
 assert.equal(nextEntryLabel(), "entry-2")
 assert.equal(nextExitLabel(),  "exit-1")
+
+if (false) { // move me down to the next section when the one above is complete
 
 ///////////////// Section 7 ///////////////////////////////////////////
 //
