1cc40cf0b3293d174ad1a3efa934d138cc0615c7
jdtynes <jadon.tynes25@gmail.com>
2020-03-01 20:44:19 -0600

    All sections complete

--- assignment.js
+++ assignment.js
@@ -47,11 +47,11 @@ Error.stackTraceLimit = 2                 // set up the environment
 
 let a = 5
 // START
-let b
-let c
-let d
-let e
-let f
+let b = 10
+let c = -11
+let d = "hawaii"
+let e = 99
+let f = 0
 // END
 
 assert.equal(a + b, 15)
@@ -62,10 +62,8 @@ assert.equal(d + a + f, "hawaii50")
 
 // add parentheses to the first parameter to make this pass
 // START
-assert.equal(d + b + c, "hawaii-1")
+assert.equal(d + (b + c), "hawaii-1")
 //
-
-if (false) { // move me down to the next section when the one above is complete
 
 ///////////////// Section 2 ///////////////////////////////////////////
 //
@@ -74,28 +72,24 @@ if (false) { // move me down to the next section when the one above is complete
 // START
 let sum = 0
 for (let i = 1; i <= 10; i++) {
-  let sum = sum + i
+  sum = sum + i
 }
 // END
 assert.equal(sum, 55)
 
-
-///////////////// Section 3 ///////////////////////////////////////////
+  ///////////////// Section 3 ///////////////////////////////////////////
 //
 // fix the `for` line of this code
 
 let result = []
 //START
-for (let i in [1,2,3,4]) {
+for (var i of [1,2,3,4]) {
 // END
   result.push(3*i)
 }
 assert.deepEqual(result, [3,6,9,12])
 
-
-
-
-///////////////// Section 4 ///////////////////////////////////////////
+  ///////////////// Section 4 ///////////////////////////////////////////
 //
 // fix the body of the loop
 
@@ -111,20 +105,21 @@ let populations = {
 let total = 0
 for (let city in  populations) {
   //START
-  total += populations.city
+  total += populations[city]
   // END
 }
 assert.equal(total, 25_145_561)
 
-
-///////////////// Section 5 ///////////////////////////////////////////
+  ///////////////// Section 5 ///////////////////////////////////////////
 //
 // write the body of the following function
 
 let count = 0
 function nextLabel(label) {
   // START
-  // ...
+  var result = label + "-" + count
+  count++
+  return result
   // END
 }
 
@@ -133,8 +128,7 @@ assert.equal(nextLabel("entry"), "entry-1")
 assert.equal(nextLabel("entry"), "entry-2")
 assert.equal(nextLabel("exit"),  "exit-3")
 
-
-///////////////// Section 6 ///////////////////////////////////////////
+  ///////////////// Section 6 ///////////////////////////////////////////
 //
 // The problem with `nextLabel` is that it uses a global
 // variable for the count, so no matter which label you
@@ -145,7 +139,12 @@ assert.equal(nextLabel("exit"),  "exit-3")
 
 function labelMaker(label) {
   // START
-  // ...
+  var count = 0
+  return function() {
+    var result = label + "-" + count
+    count++
+    return result
+  }
   // END
 }
 
@@ -158,7 +157,7 @@ assert.equal(nextExitLabel(),  "exit-0")
 assert.equal(nextEntryLabel(), "entry-2")
 assert.equal(nextExitLabel(),  "exit-1")
 
-///////////////// Section 7 ///////////////////////////////////////////
+  ///////////////// Section 7 ///////////////////////////////////////////
 //
 // The client likes your labelMaker function,
 // but wants to be able to change the next
@@ -176,7 +175,15 @@ assert.equal(nextExitLabel(),  "exit-1")
 
 function labelMaker1(label) {
   // START
-  // ...
+  var count = 0
+  return function(newCount = undefined) { // optional param, def = undefined
+    if (newCount != undefined)
+      count = newCount
+
+    result = label + "-" + count
+    count++
+    return result
+  }
   // END
 }
 
@@ -192,4 +199,5 @@ assert.equal(nextExitLabel1(),    "exit-201")
 assert.equal(nextExitLabel1(0),   "exit-0")
 assert.equal(nextExitLabel1(),    "exit-1")
 
+if (false) { // move me down to the next section when the one above is complete
 }           // end of `if (false)`
