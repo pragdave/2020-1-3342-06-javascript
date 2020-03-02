18abc71b707b6df927ebbdf592a557e6224930a8
AbrahamA00 <46973419+AbrahamA00@users.noreply.github.com>
2020-03-02 15:38:04 -0600

    sections 1-4

--- assignment.js
+++ assignment.js
@@ -47,11 +47,11 @@ Error.stackTraceLimit = 2                 // set up the environment
 
 let a = 5
 // START
-let b
-let c
-let d
-let e
-let f
+let b = 10
+let c = -11
+let d = "hawaii"
+let e = 99
+let f = 0
 // END
 
 assert.equal(a + b, 15)
@@ -62,10 +62,10 @@ assert.equal(d + a + f, "hawaii50")
 
 // add parentheses to the first parameter to make this pass
 // START
-assert.equal(d + b + c, "hawaii-1")
+assert.equal(d + (b + c), "hawaii-1")
 //
 
-if (false) { // move me down to the next section when the one above is complete
+
 
 ///////////////// Section 2 ///////////////////////////////////////////
 //
@@ -74,7 +74,7 @@ if (false) { // move me down to the next section when the one above is complete
 // START
 let sum = 0
 for (let i = 1; i <= 10; i++) {
-  let sum = sum + i
+  sum = sum + i
 }
 // END
 assert.equal(sum, 55)
@@ -86,7 +86,7 @@ assert.equal(sum, 55)
 
 let result = []
 //START
-for (let i in [1,2,3,4]) {
+for (let i of [1,2,3,4]) {
 // END
   result.push(3*i)
 }
@@ -111,10 +111,12 @@ let populations = {
 let total = 0
 for (let city in  populations) {
   //START
-  total += populations.city
+  total += populations[city]
   // END
 }
 assert.equal(total, 25_145_561)
+
+if (false) { // move me down to the next section when the one above is complete
 
 
 ///////////////// Section 5 ///////////////////////////////////////////
