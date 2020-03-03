a5f9535716336704ce08a38e33ea345e3d52c61e
Benjamin Hathoot <42647000+Bhathoot@users.noreply.github.com>
2020-03-03 15:05:49 -0600

    Compeleted section 1,2,and3
    Benjamin Hathoot 47552457

--- assignment.js
+++ assignment.js
@@ -47,11 +47,11 @@ Error.stackTraceLimit = 2                 // set up the environment
 
 let a = 5
 // START
-let b
-let c
-let d
-let e
-let f
+let b = 10
+let c = -11
+let d = 'hawaii'
+let e = 99
+let f =  0
 // END
 
 assert.equal(a + b, 15)
@@ -62,10 +62,10 @@ assert.equal(d + a + f, "hawaii50")
 
 // add parentheses to the first parameter to make this pass
 // START
-assert.equal(d + b + c, "hawaii-1")
+assert.equal(d + (b + c), "hawaii-1")
 //
 
-if (false) { // move me down to the next section when the one above is complete
+ // move me down to the next section when the one above is complete
 
 ///////////////// Section 2 ///////////////////////////////////////////
 //
@@ -74,7 +74,7 @@ if (false) { // move me down to the next section when the one above is complete
 // START
 let sum = 0
 for (let i = 1; i <= 10; i++) {
-  let sum = sum + i
+  sum = sum + i
 }
 // END
 assert.equal(sum, 55)
@@ -86,19 +86,19 @@ assert.equal(sum, 55)
 
 let result = []
 //START
-for (let i in [1,2,3,4]) {
+for (let i of [1,2,3,4]) {
 // END
   result.push(3*i)
 }
-assert.equal(result, [3,6,9,12])
+assert.deepEqual(result, [3,6,9,12])
 
 
 
-
+if (false) {
 ///////////////// Section 4 ///////////////////////////////////////////
 //
 // fix the body of the loop
-
+/*
 let populations = {
   houston:     2_099_451,
   san_antonio: 1_327_407,
@@ -107,14 +107,14 @@ let populations = {
   fort_worth:    741_206,
   rest:       18_989_291,
 }
-
+*/
 let total = 0
 for (let city in  populations) {
   //START
   total += populations.city
   // END
 }
-assert.equal(total, 25_145_561)
+//assert.equal(total, 25_145_561)
 
 
 ///////////////// Section 5 ///////////////////////////////////////////
