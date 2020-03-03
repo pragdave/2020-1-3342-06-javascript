bcf3ec6ce48edf04f3364af1b24eb65686904e80
Sam Walsh <samwalsh@Sams-MacBook-Pro.local>
2020-03-03 16:15:12 -0600

    third section completed

--- assignment.js
+++ assignment.js
@@ -79,7 +79,7 @@ for (let i = 1; i <= 10; i++) {
 // END
 assert.equal(sum,55)
 
-if (false) { // move me down to the next section when the one above is complete
+
 
 ///////////////// Section 3 ///////////////////////////////////////////
 //
@@ -87,13 +87,13 @@ if (false) { // move me down to the next section when the one above is complete
 
 let result = []
 //START
-for (let i in [1,2,3,4]) {
+for (let i of [1,2,3,4]) { //changed the in to an of
 // END
   result.push(3*i)
 }
 assert.deepEqual(result, [3,6,9,12])
 
-
+if (false) { // move me down to the next section when the one above is complete
 
 
 ///////////////// Section 4 ///////////////////////////////////////////
